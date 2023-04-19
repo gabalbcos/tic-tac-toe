870fbc588109d6b57bc9f5203c7fdde72c2dd821
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-04-19 16:02:29 -0300

    add gameboard module js

---
+++ script.js
@@ -0,0 +1,66 @@
+
+  let board = [['','',''],['','',''],['','','']];
+  let round = 0;
+
+  // Winning combinations
+ const winningCombinations = [
+  // Rows
+  [[0, 0], [0, 1], [0, 2]],
+  [[1, 0], [1, 1], [1, 2]],
+  [[2, 0], [2, 1], [2, 2]],
+  // Columns
+  [[0, 0], [1, 0], [2, 0]],
+  [[0, 1], [1, 1], [2, 1]],
+  [[0, 2], [1, 2], [2, 2]],
+  // Diagonals
+  [[0, 0], [1, 1], [2, 2]],
+  [[0, 2], [1, 1], [2, 0]]
+  ];
+
+  // function to add the symbol
+  const addSignal = (row, column) => {
+    if (board[row][column] === ''){
+      board[row][column] = playerSignal();
+      round += 1;
+    } else { 
+      return };
+  }
+
+  // function to dictate the player turn
+  const playerTurn = () => {
+      return (round % 2 === 0) ;
+  }
+
+  // function to change the player turn
+  const playerSignal = () => {
+    if(playerTurn()) {
+      return 'X';
+    } else {
+      return 'O';
+    }
+  }
+
+  const checkWinner = (player) => {
+      for (let combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (board[a[0]][a[1]] === player && board[b[0]][b[1]] === player && board[c[0]][c[1]] === player) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+  const finalMessage = () => {
+      if (checkWin('X')) {
+      console.log('Player X wins!');
+    } else {
+      console.log('No winner yet.');
+    }
+  }
+  
+  // function to log the board
+  const getBoard = () => {
+    console.log(board);
+  }
+
+ 
