3a1fdbf9ab92fd8d7047dc3fcd9ada5ba849e471
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-04-22 19:46:11 -0300

    add click functionality

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 // Attempt 2 
 const Gameboard = (() => {
-  let gameboard =['','',,'','','','','','','',];
+  let gameboard =['','','','','','','','','',];
 
   const render = () => {
     let boardHTML = '';
     gameboard.forEach((square, index ) => {
       boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
+      
     })
     document.querySelector('#board-grid').innerHTML = boardHTML;
     const squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
       square.addEventListener('click', Game.handleClick);
-    });
+    })
+  }
+
+  const update = (index, value) => {
+    gameboard[index] = value;
+    render();
   }
 
   return {render,
+    update,
          }
 })();
 
@@ -39,10 +46,16 @@ const Game = (() => {
     currentPlayerIndex = 0;
     gameOver = false;
     Gameboard.render();
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+      square.addEventListener('click', Game.handleClick);
+    })
   }
 
   const handleClick = (event) => {
-    console.log(event.target.id);
+    let index = event.target.id.replace('square-',"");
+    Gameboard.update(index, players[currentPlayerIndex].mark);
+    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
   }
 
   return{start, handleClick};
