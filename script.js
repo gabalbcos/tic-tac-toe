77ae99ca4da7bb34394faa579926308074f89ef4
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-04-23 11:20:40 -0300

    add final functionality to game - will redo later

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // Attempt 2 
+const displayController = (() => {
+  const renderMessage = (message) => {
+    document.querySelector('#round').innerHTML = message;
+  }
+
+  return {renderMessage}
+})();
+
 const Gameboard = (() => {
   let gameboard =['','','','','','','','','',];
 
@@ -20,8 +28,11 @@ const Gameboard = (() => {
     render();
   }
 
+  const getGameboard = () => gameboard;
+
   return {render,
     update,
+    getGameboard,
          }
 })();
 
@@ -53,16 +64,66 @@ const Game = (() => {
   }
 
   const handleClick = (event) => {
+    if (gameOver) {
+      return;
+    }
     let index = event.target.id.replace('square-',"");
+    if (Gameboard.getGameboard()[index] !== ""){
+      return;
+    }
     Gameboard.update(index, players[currentPlayerIndex].mark);
+
+    if (checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].mark)) {
+      gameOver = true;
+      displayController.renderMessage(`${players[currentPlayerIndex].name} WON!`);
+    } else if (checkForTie(Gameboard.getGameboard())) {
+      gameOver = true;
+      displayController.renderMessage("It's a TIE");
+    }
     currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
   }
 
-  return{start, handleClick};
+  function checkForWin(board) {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+]
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (board[a] && board[a] === board[b] && board[a] === board[c]){
+      return true;
+      }  
+    }  
+    return false;
+}
+
+function checkForTie(board) {
+    return board.every(cell => cell !== '')
+}
+
+  const restart = () => {
+    for (let i = 0; i < 9; i += 1){
+      Gameboard.update(i, "");
+    }
+    Gameboard.render();
+  }
+
+  return{start, handleClick, restart, checkForWin};
 })();
 
+const buttonReset= document.querySelector('#btn-reset') ;
+buttonReset.addEventListener('click', () => {
+  Game.restart();
+});
 
-const buttonPlay = document.querySelector('#btn-reset') ;
+
+const buttonPlay = document.querySelector('#btn-ai') ;
 buttonPlay.addEventListener('click', () => {
   Game.start() ;
 });
